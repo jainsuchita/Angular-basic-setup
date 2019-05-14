dd0376c28355fa76da8b2374a13bdb8338f3754e
jainsuchi21 <jainsuchi21@gmail.com>
2019-05-14 14:18:20 +0530

    Initial Commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  public getUsers() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+  }
+
+  public getUser(userId) {
+    return this.http.get('https://jsonplaceholder.typicode.com/users/' + userId)
+  }
+
+  public getPosts() {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  }
+}
